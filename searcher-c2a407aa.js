52428f762e86320081e0b19bbda188fa7f91771c
dn32 <dn32@users.noreply.github.com>
2026-04-15 15:37:28 +0000

    deploy: 747d5ba36f6678285a75be52179547cf3c5cc0e6

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a3947041.js',
+                path_to_root + 'searchindex-fb4bc72d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
